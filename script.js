f6b19608c7753426cbed0eed364e0fc2cebcef90
Agnes Chueng <agneschueng@gmail.com>
2020-09-27 20:48:33 -0500

    added arrays of all possible characters to include

--- script.js
+++ script.js
@@ -21,3 +21,9 @@ var userLc = confirm("Do you want to include lowercase characters?");
 var userUc = confirm("Do you want to include uppercase characters?");
 var userNum = confirm("Do you want to include numbers?");
 var userSc = confirm("Do you want to include special characters? Special characters include: !?@#$%/'-_~().:,");
+
+// All possible characters
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var num = ["1","2","3","4","5","6","7","8","9","0"];
+var special = ["!","?","@","#","$","%","/","'","-","_","~","(",")",".",":",","];
