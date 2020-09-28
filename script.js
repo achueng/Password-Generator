cd9039f4f4d392c557993407443c22cdd53b5e7a
Agnes Chueng <agneschueng@gmail.com>
2020-09-27 20:55:19 -0500

    declared functions in charge of looking for a random character within the arrays

--- script.js
+++ script.js
@@ -27,3 +27,17 @@ var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q"
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var num = ["1","2","3","4","5","6","7","8","9","0"];
 var special = ["!","?","@","#","$","%","/","'","-","_","~","(",")",".",":",","];
+
+// Functions that generate a random number within the length of the given array, and find the element corresponding to the index of that number in the same array
+function randomLower () {
+  return lower[Math.floor(Math.random()*lower.length)];
+}
+function randomUpper () {
+  return upper[Math.floor(Math.random()*upper.length)];
+}
+function randomNum () {
+  return num[Math.floor(Math.random()*num.length)];
+}
+function randomSpecial () {
+  return special[Math.floor(Math.random()*special.length)];
+}
